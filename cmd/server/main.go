fdc10d5eacbdd0ab3c6efb047a7283ac67a73604
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2024-02-24 19:40:42 +0200

    application cmd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/maxzhovtyj/card-validator/internal/app"
 
+func main() {
+	app.Run()
 }
---
+++ internal/app/app.go
@@ -0,0 +1,48 @@
+package app
+
+import (
+	"flag"
+	"github.com/maxzhovtyj/card-validator/internal/config"
+	"github.com/maxzhovtyj/card-validator/internal/handler/handler"
+	"github.com/maxzhovtyj/card-validator/internal/service"
+	"github.com/maxzhovtyj/card-validator/pkg/log/applogger"
+	"google.golang.org/grpc"
+	"net"
+)
+
+var configPath = flag.String("configPath", "", "path to config file")
+
+func Run() {
+	logger := applogger.New()
+
+	logger.Infof("init application config")
+	flag.Parse()
+
+	if *configPath == "" {
+		logger.Fatalf("empty config path")
+	}
+
+	cfg, err := config.New(*configPath)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	cfg.LogAll()
+
+	logger.Infof("init tcp listener")
+	tcp, err := net.Listen("tcp", cfg.Addr)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Infof("init services")
+	s := service.New()
+
+	logger.Infof("init grpc server")
+	srv := grpc.NewServer()
+	handler.NewServer(srv, s, logger)
+
+	logger.Infof("start grcp server on address '%s'", cfg.Addr)
+	if err = srv.Serve(tcp); err != nil {
+		logger.Fatal(err)
+	}
+}
